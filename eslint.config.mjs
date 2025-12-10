b15264b56e864ebca7940c198708d250e6b3e59a
Marco Pavan <marcopavan.mp@gmail.com>
2025-12-10 16:18:46 +0100

    Added ESLint

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import { defineConfig } from 'eslint/config'
+
+const rules = {
+  eqeqeq: ['error', 'always'],
+  semi: ['error', 'never'],
+  quotes: ['error', 'single'],
+  indent: ['error', 2],
+  'comma-dangle': ['error', 'never'],
+  'linebreak-style': ['error', 'unix'],
+  'no-var': 'error',
+  'prefer-const': 'warn',
+  'no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_', caughtErrorsIgnorePattern: '^_' }],
+  'no-debugger': 'error',
+  'space-before-function-paren': ['error', 'never'],
+  'object-curly-spacing': ['error', 'always'],
+  'arrow-spacing': ['error', { before: true, after: true }],
+  'no-console': 'warn'
+}
+
+
+export default defineConfig([
+  { files: ['**/*.{js,mjs,cjs}'], plugins: { js }, extends: ['js/recommended'], rules: rules },
+  { files: ['**/*.{js,mjs,cjs}'], languageOptions: { globals: globals.node } },
+  { files: ['app/scripts/**/*.{js,mjs,cjs}', 'app/views/**/*.{js,mjs,cjs}'], languageOptions: { globals: globals.browser } }
+])
