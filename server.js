f92d3735c73f24704d91ab60408fc366032fe88b
Marco Pavan <marcopavan.mp@gmail.com>
2025-12-11 10:35:41 +0100

    Added: Compression Security HTTPS redirect no WWW redirect no url trailing slash

--- server.js
+++ server.js
@@ -6,13 +6,67 @@ const app = express()
 import crypto from 'crypto'
 // Generate a nonce per request for CSP
 app.use((req, res, next) => {
-  res.locals.nonce = crypto.randomUUID()
+  res.locals.nonce = crypto.randomBytes(16).toString('base64')
   next()
 })
 
 // Parse application/json
 app.use(express.json())
 
+
+// Compression middleware
+import compression from 'compression'
+app.use(compression({ filter: shouldCompress }))
+function shouldCompress(req, res) {
+  if (req.headers['x-no-compression']) {
+    // don't compress responses with this request header
+    return false
+  }
+  // fallback to standard filter function
+  return compression.filter(req, res)
+}
+
+// Security headers middleware
+app.use((req, res, next) => {
+  res.setHeader('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload')
+  res.setHeader('Upgrade-insecure-requests', '1')
+  // eslint-disable-next-line max-len
+  res.setHeader('Content-Security-Policy', `default-src 'none'; script-src 'self' 'nonce-${res.locals.nonce}'; style-src 'self' 'unsafe-inline'; img-src 'self'; object-src 'none'; frame-src 'self'; form-action 'self'; font-src 'self'; media-src 'self'; connect-src 'self' https://c.statcounter.com; frame-ancestors 'none'; base-uri 'none'`)
+  res.setHeader('X-Content-Type-Options', 'nosniff')
+  next()
+})
+
+// HTTPS redirect middleware
+app.use((req, res, next) => {
+  if (process.env.NODE_ENV === 'production' && process.env.FORCE_HTTPS === 'true') {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect('https://' + req.headers.host + req.url)
+    } else { return next() }
+  } else { return next() }
+})
+
+// WWW redirect middleware
+app.set('trust proxy', true)
+app.use((req, res, next) => {
+  if (req.headers.host && req.headers.host.slice(0, 4) === 'www.') {
+    const newHost = req.headers.host.slice(4)
+    return res.redirect(301, req.protocol + '://' + newHost + req.originalUrl)
+  }
+  next()
+})
+
+// Trailing slash removal middleware
+app.use((req, res, next) => {
+  const { path, query } = req
+  if (path !== '/' && path.endsWith('/')) {
+    const newPath = path.slice(0, -1)
+    const qs = Object.keys(query).length
+      ? '?' + new URLSearchParams(query).toString()
+      : ''
+    return res.redirect(301, newPath + qs)
+  }
+  next()
+})
 
 // Set view engine
 import path from 'path'
@@ -53,7 +107,11 @@ app.use((req, res) => {
 })
 // 500 handling
 app.use((err, _req, res, _next) => {
-  console.error(err.stack)
+  if (process.env.NODE_ENV !== 'production') {
+    console.error(err.stack)
+  } else {
+    console.error('An internal server error occurred.')
+  }
   res.status(500).render('5xx/500')
 })
 
